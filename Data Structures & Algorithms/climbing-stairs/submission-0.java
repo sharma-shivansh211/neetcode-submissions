42d11203f734bd995f9c864433596a5e2953ee28
sharma-shivansh211 <shivansh.sharma.study@gmail.com>
2026-06-05 19:40:41 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        return helper(n, dp);
+    }
+    static int helper( int n, int[] dp){
+        if(n == 1 || n == 0) return 1;
+        if(dp[n] != 0) return dp[n];
+        dp[n] =  helper(n-1,dp) + helper(n-2,dp);
+        return dp[n];
+    }
+}
